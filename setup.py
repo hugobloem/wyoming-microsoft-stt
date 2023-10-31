df54013fe67dd344504b7060dc6b4c3a34b1234f
Hugo Bloem <hugobloem@gmail.com>
2023-10-31 14:25:36 +0000

    add docker setup

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+import setuptools
+from setuptools import setup
+
+this_dir = Path(__file__).parent
+module_dir = this_dir / "wyoming-microsoft-stt
+
+requirements = []
+requirements_path = this_dir / "requirements.txt"
+if requirements_path.is_file():
+    with open(requirements_path, "r", encoding="utf-8") as requirements_file:
+        requirements = requirements_file.read().splitlines()
+
+data_files = [module_dir / "languages.json"]
+
+# -----------------------------------------------------------------------------
+
+setup(
+    name="wyoming-microsoft-stt",
+    version="1.0.0",
+    description="Wyoming Server for Microsoft STT",
+    url="https://github.com/hugobloem/wyoming-microsoft-stt",
+    author="Hugo Bloem",
+    author_email="",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    package_data={
+        "wyoming-microsoft-stt": [str(p.relative_to(module_dir)) for p in data_files]
+    },
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="rhasspy wyoming microsft stt",
+)
