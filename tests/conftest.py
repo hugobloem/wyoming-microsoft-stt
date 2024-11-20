4836ec9740658f1a7f0e702f65a25f96b0e65b97
Hugo <hugobloem@gmail.com>
2024-11-20 09:46:45 +0000

    refactor: update MicrosoftSTT fixture to return arguments instead of instance

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,14 @@
 
 from types import SimpleNamespace
 import pytest
-from wyoming_microsoft_stt.microsoft_stt import MicrosoftSTT
 import os
 
 
 @pytest.fixture
-def microsoft_stt():
+def microsoft_stt_args():
     """Return MicrosoftSTT instance."""
     args = SimpleNamespace(
         subscription_key=os.environ.get("SPEECH_KEY"),
         service_region=os.environ.get("SPEECH_REGION"),
     )
-    return MicrosoftSTT(args)
+    return args
