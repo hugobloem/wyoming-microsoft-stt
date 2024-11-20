299b3fec41d320a154624b3d9928c4cc4fd68e54
Hugo <42470993+hugobloem@users.noreply.github.com>
2024-11-20 09:48:12 +0000

    refactor: update MicrosoftSTT fixture to return arguments instead of instance (#64)

--- tests/test_microsoft_stt.py
+++ tests/test_microsoft_stt.py
@@ -1,13 +1,18 @@
 """Tests for the MicrosoftTTS class."""
 
+from wyoming_microsoft_stt.microsoft_stt import MicrosoftSTT
 
-def test_initialize(microsoft_stt):
+
+def test_initialize(microsoft_stt_args):
     """Test initialization."""
+    microsoft_stt = MicrosoftSTT(microsoft_stt_args)
     assert microsoft_stt.speech_config is not None
 
 
-def test_transcribe(microsoft_stt):
+def test_transcribe(microsoft_stt_args):
     """Test synthesize."""
+    microsoft_stt = MicrosoftSTT(microsoft_stt_args)
+
     filename = "./tests/hello_world.wav"
     language = "en-GB"
 
