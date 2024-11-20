22ddd068262eb85484bc1c45cd537aaaa4454d4f
Ryan Holt <ryan@ryanholt.net>
2024-11-19 22:14:48 -0500

    additional changes

--- wyoming_microsoft_stt/microsoft_stt.py
+++ wyoming_microsoft_stt/microsoft_stt.py
@@ -11,6 +11,7 @@ class MicrosoftSTT:
         """Initialize."""
         self.args = args
         try:
+            # Initialize the speech configuration with the provided subscription key and region
             self.speech_config = speechsdk.SpeechConfig(
                 subscription=args.subscription_key, region=args.service_region
             )
@@ -21,21 +22,24 @@ class MicrosoftSTT:
 
     def transcribe(self, filename: str, language=None):
         """Transcribe a file."""
+        # Use the default language from args if no language is provided
         if language is None:
             language = self.args.language
 
         try:
+            # Configure audio input for speech recognition
             audio_config = speechsdk.audio.AudioConfig(filename=filename)
-            # Creates a speech recognizer using a file as audio input, also specify the speech language
+            # Create a speech recognizer with the configured speech and audio settings
             speech_recognizer = speechsdk.SpeechRecognizer(
                 speech_config=self.speech_config,
                 language=language,
                 audio_config=audio_config,
             )
 
+            # Perform recognition on the audio file
             result = speech_recognizer.recognize_once()
 
-            # Check the result
+            # Check the result and return the recognized text or log appropriate warnings
             if result.reason == speechsdk.ResultReason.RecognizedSpeech:
                 _LOGGER.debug(f"Recognized: {result.text}")
                 return result.text
