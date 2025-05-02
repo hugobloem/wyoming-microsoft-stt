8ae445cdbde4ac2f94948ad57abbc564bccbcd32
Hugo Bloem <hugobloem@gmail.com>
2025-05-02 22:51:28 +0100

    refactor(tests): remove unused transcribe test function

--- tests/test_microsoft_stt.py
+++ tests/test_microsoft_stt.py
@@ -9,20 +9,6 @@ def test_initialize(microsoft_stt_args):
     assert microsoft_stt.speech_config is not None
 
 
-def test_transcribe(microsoft_stt_args):
-    """Test synthesize."""
-    assert True
-    return  ## not sure what the best way to test this is
-
-    microsoft_stt = MicrosoftSTT(microsoft_stt_args)
-
-    filename = "./tests/hello_world.wav"
-    language = "en-GB"
-
-    result = microsoft_stt.transcribe(filename, language)
-    assert "hello world" in result.lower()
-
-
 def test_set_profanity(microsoft_stt_args):
     """Test set_profanity."""
     microsoft_stt = MicrosoftSTT(microsoft_stt_args)
